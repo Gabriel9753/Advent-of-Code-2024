c447fa0a6d3a543701c83e663cc95260d5dd2086
Gabriel9753 <gabriel.elias2000@outlook.de>
2024-12-05 09:54:54 +0100

    Init day 5

---
+++ src/day_05/solution.py
@@ -0,0 +1,81 @@
+import math
+import os
+import re
+import sys
+from collections import Counter, OrderedDict, defaultdict, deque, namedtuple
+from datetime import datetime
+from functools import lru_cache, reduce
+from itertools import chain, combinations, permutations, product
+
+import numpy as np
+from tqdm import tqdm
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+par_dir = os.path.dirname(cur_dir)
+sys.path.append(par_dir)
+
+from util.general_util import average_time, load_input, timer, write_times_to_readme
+
+last_dir = str(os.path.basename(os.path.normpath(cur_dir)))
+cur_day = re.findall(r"\d+", last_dir)
+cur_day = int(cur_day[0]) if len(cur_day) > 0 else datetime.today().day
+images_path = os.path.join(par_dir, "images")
+
+
+@timer(return_time=True)
+def task1(day_input):
+    day_input = day_input.split("\n\n")
+    costs = defaultdict(lambda: -1)
+    rules = day_input[0].split("\n")
+    prints = day_input[1].split("\n")
+
+    cur_cost = 0
+
+    for rule in rules:
+        rule = rule.split("|")
+        rule = [int(x) for x in rule]
+
+        # cost for rule[1] must be higher than rule[0]
+        if rule[1] > rule[0]:
+            costs[rule[1]] = rule[0]
+
+    print(costs)
+
+
+@timer(return_time=True)
+def task2(day_input):
+    # Day-specific code for Task 2
+    pass
+
+
+def main():
+    # Choose between the real input or the example input
+    # day_input = load_input(os.path.join(cur_dir, "input.txt"))
+    day_input = load_input(os.path.join(cur_dir, "example_input.txt"))
+
+    # Call the tasks and store their results (if needed)
+    result_task1, time_task1 = task1(day_input)
+    result_task2, time_task2 = task2(day_input)
+
+    print(f"\nDay {cur_day}")
+    print("------------------")
+    # Print the results
+    print("\nAnswers:")
+    print(f"Task 1: {result_task1}")
+    print(f"Task 2: {result_task2}")
+
+    print("\nTimes:")
+    print(f"Task 1: {time_task1:.6f} seconds")
+    print(f"Task 2: {time_task2:.6f} seconds")
+
+    # 100 times and average the time
+    # avg_time_task1 = average_time(100, task1, day_input)
+    # avg_time_task2 = average_time(100, task2, day_input)
+    # print("\nAverage times:")
+    # print(f"Task 1: {avg_time_task1:.6f} seconds")
+    # print(f"Task 2: {avg_time_task2:.6f} seconds")
+    # write_times_to_readme(cur_day, avg_time_task1, avg_time_task2)
+
+
+if __name__ == "__main__":
+    main()
