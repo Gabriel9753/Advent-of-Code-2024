d483e4809bd3f60f35e98d02301dd85ddefa4550
Gabriel9753 <gabriel.elias2000@outlook.de>
2024-12-12 11:04:28 +0100

    Add unsolved day 12

---
+++ src/day_12/solution.py
@@ -0,0 +1,76 @@
+import math
+import os
+import re
+import sys
+from collections import Counter, OrderedDict, defaultdict, deque, namedtuple
+from datetime import datetime
+from functools import lru_cache, reduce
+from itertools import chain, combinations, permutations, product
+import argparse
+
+import numpy as np
+from tqdm import tqdm
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+par_dir = os.path.dirname(cur_dir)
+sys.path.append(par_dir)
+
+from util.general_util import average_time, load_input, timer, write_times_to_readme
+
+last_dir = str(os.path.basename(os.path.normpath(cur_dir)))
+cur_day = re.findall(r"\d+", last_dir)
+cur_day = int(cur_day[0]) if len(cur_day) > 0 else datetime.today().day
+images_path = os.path.join(par_dir, "images")
+
+
+@timer(return_time=True)
+def preprocess_input(input_data):
+    # Preprocess the input data (if needed)
+    # 2d numpy map
+    # return np.array([[x for x in row] for row in input_data.splitlines()])
+    return {(x, y): z for y, row in enumerate(input_data.strip().splitlines()) for x, z in enumerate(row)}
+
+
+@timer(return_time=True)
+def task1(day_input):
+    print(day_input)
+    # gehe über alle Koordinaten, die bisher noch nicht besucht werden.
+    # Für jede Koordinate, "beobachte" die Ausbreitung der Pflanzen, solange sich die Pflanzenart nicht ändert
+
+@timer(return_time=True)
+def task2(day_input):
+    # Day-specific code for Task 2
+    pass
+
+
+def main(args):
+    # Choose between the real input or the example input
+    if args.example:
+        day_input = load_input(os.path.join(cur_dir, "example_input.txt"))
+    else:
+        day_input = load_input(os.path.join(cur_dir, "input.txt"))
+
+    day_input, t = preprocess_input(day_input)
+    result_task1, time_task1 = task1(day_input)
+    result_task2, time_task2 = task2(day_input)
+
+    print(f"\nDay {cur_day}")
+    print("------------------")
+    print(f"Processing data: {t:.6f} seconds")
+    print(f"Task 1: {result_task1} ({time_task1:.6f} seconds)")
+    print(f"Task 2: {result_task2} ({time_task2:.6f} seconds)")
+
+    if args.timeit:
+        avg_time_task1 = average_time(100, task1, day_input)
+        avg_time_task2 = average_time(100, task2, day_input)
+        print("\nAverage times:")
+        print(f"Task 1: {avg_time_task1:.6f} seconds")
+        print(f"Task 2: {avg_time_task2:.6f} seconds")
+        write_times_to_readme(cur_day, avg_time_task1, avg_time_task2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--example", type=int, help="Use the example input", default=1)
+    parser.add_argument("--timeit", type=int, help="Average the execution time over 100 runs", default=0)
+    main(parser.parse_args())
